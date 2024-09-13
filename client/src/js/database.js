979c0bef95f4c5843b6060c3c890ee05a8d4a202
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-09-12 19:35:38 -0500

    updated

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,10 +18,10 @@ export const putDb = async (content) => {
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ value: content});
+  const request = store.put({id: 1, value: content});
   const result = await request;
 
-  console.log(`- data saved to the database with ID`, result);
+  console.log(`- data saved to the database`, result.valueOf);
 
 };
 
@@ -32,14 +32,13 @@ export const getDb = async () => {
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
 
-  result.length > 0
-  ? console.log(`- data retrieved from the db`, result) 
-  : console.log(`- no data found in the db`);    
+  result ? console.log(`- data retrieved from the db`, result.value) : 
+  console.log(`- no data found in the db`);    
 
-  return result;
+  return result?.value;
 };
 
 initdb();
